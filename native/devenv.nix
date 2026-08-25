62787e31668bfd1cc4c0cb551b24362c0d02c1a0
TheFreeDuck <fredrikthorcewers@gmail.com>
2026-08-25 08:23:40 +0200

    chore(native): update devenv.nix with C build tools and libs

--- native/devenv.nix
+++ native/devenv.nix
@@ -1,46 +1,30 @@
-{ pkgs, lib, config, inputs, ... }:
+{pkgs, ...}: {
+  packages = with pkgs; [
+    cmake
+    ninja
+    clang
+    llvmPackages.clang-tools
+    llvmPackages.bintools
+    valgrind
+    conan
+    pkg-config
 
-{
-  # https://devenv.sh/basics/
-  env.GREET = "devenv";
+    #C libs
+    cjson
+    libharu
 
-  # https://devenv.sh/packages/
-  packages = [ pkgs.git ];
+    just
+    doxygen
+  ];
 
-  # https://devenv.sh/languages/
-  # languages.rust.enable = true;
+  languages.c.enable = true;
 
-  # https://devenv.sh/processes/
-  # processes.dev.exec = "${lib.getExe pkgs.watchexec} -n -- ls -la";
-
-  # https://devenv.sh/services/
-  # services.postgres.enable = true;
-
-  # https://devenv.sh/scripts/
-  scripts.hello.exec = ''
-    echo hello from $GREET
-  '';
-
-  # https://devenv.sh/basics/
   enterShell = ''
-    hello         # Run scripts directly
-    git --version # Use packages
+    if [ ! -f "build/default/build.ninja" ]; then
+        cmake --preset default
+      fi
+    if [ -f "build/default/compile_commands.json" ]; then
+       ln -sf build/default/compile_commands.json .
+    fi
   '';
-
-  # https://devenv.sh/tasks/
-  # tasks = {
-  #   "myproj:setup".exec = "mytool build";
-  #   "devenv:enterShell".after = [ "myproj:setup" ];
-  # };
-
-  # https://devenv.sh/tests/
-  enterTest = ''
-    echo "Running tests"
-    git --version | grep --color=auto "${pkgs.git.version}"
-  '';
-
-  # https://devenv.sh/git-hooks/
-  # git-hooks.hooks.shellcheck.enable = true;
-
-  # See full reference at https://devenv.sh/reference/options/
 }
