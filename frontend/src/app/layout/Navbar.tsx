093591a720c0652ca7bcf6235ca50117b25f7131
oliviaaMach <olivia.mach@outlook.com>
2026-08-31 08:12:14 +0200

    npm i tanstack and small changes in navbar

--- frontend/src/app/layout/Navbar.tsx
+++ frontend/src/app/layout/Navbar.tsx
@@ -1,9 +1,5 @@
 import { NavLink } from "react-router-dom"
 
-type NavbarProps = {
-    className?: string;
-}
-
 const navLinks = [
     {
         name: "Översikt",
@@ -11,7 +7,7 @@ const navLinks = [
     },
         {
         name: "Konton",
-        to: "/dashboard" ,
+        to: "/transactions" ,
     },
     {
         name: "Skatterapporter",
@@ -28,15 +24,13 @@ const navLinks = [
 ]
 
 
-export default function Navbar({
-    className=""
-}: NavbarProps) {
+export default function Navbar() {
     return (
         <nav className="lg:border-r border-border lg:col-span-2 p-6">
             <h2 className="text-brand">nordiska<span className="text-accent">.</span></h2>
             <ul className="pt-6 text-medium">
                 {navLinks.map((navLink) => (
-                    <li key={navLink.to} className={`w-full ${className}`}>
+                    <li key={navLink.to} className= "w-full">
                         <NavLink to={navLink.to} className={({ isActive }) => 
                             `flex w-full p-3 mt-1 rounded-default transition-colors duration-150 ${isActive 
                             ? "bg-brand text-white"
