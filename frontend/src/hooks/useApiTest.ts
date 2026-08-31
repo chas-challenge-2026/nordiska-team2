efe570355022b35d0a728c035bfe9f5c93c290b5
bjorkmanemelie <bjorkmanemelie@hotmail.com>
2026-08-31 07:41:46 +0200

    changes in testfiles, set up api connection

--- frontend/src/hooks/useApiTest.ts
+++ frontend/src/hooks/useApiTest.ts
@@ -2,11 +2,11 @@ import { useQuery } from '@tanstack/react-query'
 import { apiClient } from '../client'
 
 export function useApiTest() {
-    return ({
-        queryKey: ['apiTest'],
-        queryFn: async () => {
-            const response = await apiClient.get('/health')
-            return response.data
-        },
-    })
+  return useQuery({
+    queryKey: ['apiTest'],
+    queryFn: async () => {
+      const response = await apiClient.get('/health')
+      return response.data
+    },
+  })
 }
