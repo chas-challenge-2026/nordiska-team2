2af41b3e5d470364b54c28bb18605d5f6464f5a9
bjorkmanemelie <bjorkmanemelie@hotmail.com>
2026-09-02 12:13:35 +0200

    setting up loginform, with useState

--- frontend/src/features/auth/LoginPage.tsx
+++ frontend/src/features/auth/LoginPage.tsx
@@ -1,8 +1,14 @@
-
+import { useState } from "react";
 
 export default function LoginPage() {
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+    const [errorMessage, setErrorMessage] = useState( '')
+
     return ( 
-        <h1>Loginpage</h1>
+        <div>
+
+        </div>
     )
 }
 
