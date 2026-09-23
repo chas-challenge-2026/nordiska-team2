d8a754cf25095569396abc1d739be895c5162308
bjorkmanemelie <bjorkmanemelie@hotmail.com>
2026-09-23 11:46:43 +0200

    FAQ tests passed

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,11 +1,17 @@
 import react from '@vitejs/plugin-react'
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vitest/config'
 import tailwindcss from '@tailwindcss/vite'
+
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     tailwindcss(),
-  ]
+  ],
+  test: {
+    environment: 'jsdom',
+    globals: true,
+    setupFiles: ["./src/tests/setup.ts"],
+  },
 })
